5ba36de04607a374bcf29d4ca1db739a2ea2f47c
JulienG <b@b.b>
2022-04-19 11:20:03 +0200

    Ajout de la navbar

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
+        <title>Wikifruits</title>
+    </head>
+    <body>
+        <?php include 'includes/navbar.php'?>
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
